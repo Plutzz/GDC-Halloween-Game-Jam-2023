5db9b264861ce0c66f3b2e9a83bd1ae0158bd3af
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-10-13 15:59:56 -0700

    player movement
    player movement script "PlayerMovement"

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class PlayerMovement : MonoBehaviour
+{
+    //I recommend 7 for the move speed, and 1.2 for the force damping
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public Vector2 PlayerInput;
+    void Update()
+    {
+        PlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+    void FixedUpdate()
+    {
+        Vector2 moveForce = PlayerInput * moveSpeed;
+        rb.velocity = moveForce;
+    }
+ 
+}
