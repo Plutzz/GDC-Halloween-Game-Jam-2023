6dc71eea2e534867800faf4fe9976c1c6542e9b5
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-10-13 16:58:57 -0700

    bullet and spread
    EnemyBullet script

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float bulletSpread;
+    public float force;
+
+    private GameObject player;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = PlayerMovement.Instance.gameObject;
+
+        Vector3 spread = new Vector3(Random.Range(-bulletSpread, bulletSpread), Random.Range(-bulletSpread, bulletSpread), 0);
+
+        Vector3 direction = (player.transform.position + spread) - transform.position;
+
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+}
