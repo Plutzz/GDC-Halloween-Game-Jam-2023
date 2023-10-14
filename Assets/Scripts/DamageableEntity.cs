6f6fb7ef7c608f95df21b857d558942eff541533
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-10-13 17:33:42 -0700

    enemy bullets deal damage
    health bar ui prefab and bullets deal damage. damageable entity script

---
+++ Assets/Scripts/DamageableEntity.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DamageableEntity : MonoBehaviour
+{
+    public int currentHp;
+    public virtual void takeDamage(int damage)
+    {
+        currentHp -= damage;
+
+        if (currentHp <= 0)
+            OnDeath();
+    }
+    
+    public int GetCurrentHp()
+    {
+        return currentHp;
+    }
+
+    public virtual void OnDeath()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : DamageableEntity
+{
+    public int maxHp = 100;
+    public Image hpSliderFill;
+    public Slider hpBar;
+
+    public static PlayerHealth Instance;
+
+    void Awake ()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        GetComponent<Slider>();
+        currentHp = maxHp;
+    }
+
+    public override void takeDamage (int damage)
+    {
+        if(currentHp > 0)
+        {
+            currentHp -= damage;
+        }
+        
+        float fillvalue = (float)currentHp/(float)maxHp;
+
+        // Debug.Log(fillvalue);
+
+        hpBar.value = fillvalue;
+        Debug.Log("hit");
+    }
+
+}
