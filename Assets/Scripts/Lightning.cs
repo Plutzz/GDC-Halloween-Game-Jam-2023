d876148fb9f6b23742bdd1ab712dfe79a29f1c40
Plutzz <benjiongco@gmail.com>
2023-10-14 15:55:30 -0700

    Lightning Thing

--- Assets/Scripts/Lightning.cs
+++ Assets/Scripts/Lightning.cs
@@ -7,7 +7,7 @@ using UnityEngine.Rendering.Universal;
 [RequireComponent(typeof(Light2D))]
 public class Lightning : MonoBehaviour
 {
-
+    public Ease ease;
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -16,8 +16,10 @@ public class Lightning : MonoBehaviour
         }
     }
 
+    //Make sure tween finishes before starting a new tween
     public void Flash()
     {
-        GetComponent<Light2D>().DOIntensity(1000f, 0.25f).SetLoops(1, LoopType.Yoyo);
+        Debug.Log("Flash");
+        DOTween.To(()=> GetComponent<Light2D>().intensity, x => GetComponent<Light2D>().intensity = x, 2f, 0.1f).SetEase(ease).SetLoops(4, LoopType.Yoyo);
     }
 }
