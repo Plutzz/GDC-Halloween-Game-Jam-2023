6e6d9c08c4b7f0439ff7d999af6f327204bd48d6
Nathan Gardea <natecgardea@gmail.com>
2023-10-15 09:13:23 -0700

    Root Prefab

--- Assets/ClearCircle.cs
+++ Assets/ClearCircle.cs
@@ -7,7 +7,7 @@ public class ClearCircle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 0.1f);   
+        Destroy(this.gameObject, 0.5f);   
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
