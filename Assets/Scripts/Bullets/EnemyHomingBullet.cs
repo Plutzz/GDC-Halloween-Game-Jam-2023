d1aefaabf274d0de9a65f267129840b1d5e0aef3
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-10-15 13:19:17 -0700

    homing bullet timer

--- Assets/Scripts/Bullets/EnemyHomingBullet.cs
+++ Assets/Scripts/Bullets/EnemyHomingBullet.cs
@@ -8,14 +8,17 @@ public class EnemyHomingBullet : MonoBehaviour
 {
     public float force;
     public int damage;
+    public float homingTime = 1.5f;
 
     private GameObject player;
     private Rigidbody2D rb;
+    private float cooldown;
 
     // Start is called before the first frame update
     void Start ()
     {
         player = PlayerMovement.Instance.gameObject;
+        cooldown = homingTime;
         Destroy(this.gameObject, 5f);
     }
 
@@ -26,10 +29,16 @@ public class EnemyHomingBullet : MonoBehaviour
 
         float distance = force * Time.deltaTime;
 
-        transform.Translate(direction.normalized * distance, Space.World);
+        if(cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+            transform.Translate(direction.normalized * distance, Space.World);
 
-        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+            float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+        } else {
+            transform.position += transform.up * force * Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider) {
