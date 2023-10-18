4d607a53472846fb44c26c788cd6b6393406bb5f
Plutzz <benjiongco@gmail.com>
2023-10-18 14:19:29 -0700

    Finish creating base turret script

--- Assets/Scripts/BaseTurret.cs
+++ Assets/Scripts/BaseTurret.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class BaseTurret : MonoBehaviour
@@ -61,7 +62,67 @@ public class BaseTurret : MonoBehaviour
     protected virtual void Start()
     {
         isActive = false;
+        CalculateAttributes();
     }
+
+    protected void Update()
+    {
+        graphics.GetComponent<SpriteRenderer>().sprite = levelSprites[level - 1];
+
+        if (!isActive) return;
+
+        timeUntilFire += Time.deltaTime;
+        timeAlive += Time.deltaTime;
+
+
+        rangeDisplay.transform.localScale = new Vector3(targetingRange * 2, targetingRange * 2, 1f);
+        rangeDisplay.GetComponent<Light2D>().pointLightOuterRadius = targetingRange;
+
+        if (!(lifetime < 0))
+        {
+            timerBar.fillAmount = 1 - (timeAlive / lifetime);
+        }
+
+        // If building has expired destroy building and free up the grid spot
+        if (!(lifetime < 0) && timeAlive > lifetime)
+        {
+            GridBuildingSystem.Instance.destroyBuilding(GetComponent<Building>());
+            Destroy(gameObject);
+        }
+
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+        else
+        {
+
+            if (timeUntilFire >= 1f / bps)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
+    }
+
+    
+    //Make a default bullet script
+    private void Shoot()
+    {
+        GameObject _bullet = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
+        TurretBullet bulletScript = _bullet.GetComponent<TurretBullet>();
+        bulletScript.SetDamage(damage);
+        bulletScript.SetTarget(target);
+    }
+
     public static void Upgrade()
     {
         if (level == maxLevel) return;
@@ -99,6 +160,29 @@ public class BaseTurret : MonoBehaviour
         bps = CalculateBPS();
         targetingRange = CalculateRange();
         damage = CalculateDamage();
+    }
+
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+
+        Quaternion _targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, _targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private bool CheckTargetIsInRange()
+    {
+        return (Vector2.Distance(target.position, transform.position) <= targetingRange);
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemies);
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
     }
 
 
