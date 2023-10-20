7fd393801c973be6422da8906d4077bccc7ffa24
Plutzz <benjiongco@gmail.com>
2023-10-19 21:04:54 -0700

    Redo Audio System

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,10 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class SoundManager : Singleton<SoundManager>
 {
     public SoundAudioClip[] soundAudioClipsArray;
+
+
+    //private GameObject[] soundAudioClips;
+
+    [SerializeField]
+    private Queue<GameObject> soundAudioClipsQueue;
+
+    [SerializeField]
+    private int maxAudioSources = 10;
+
+    private void Start()
+    {
+        //soundAudioClips = new GameObject[maxAudioSources];
+
+        soundAudioClipsQueue = new Queue<GameObject>(maxAudioSources);
+    }
 
     public enum Sounds
     {
@@ -14,17 +31,41 @@ public class SoundManager : Singleton<SoundManager>
         Hit,
         Thunder
     }
+
+    //Use this for sounds that may be repeated very quickly Ex: a bunch of towers shooting
     public void PlaySound(Sounds _sound)
+    {
+        GameObject soundGameObject;
+        AudioSource audioSource;
+        //Create Audio Source Game Object
+        if (soundAudioClipsQueue.Count <= maxAudioSources)
+        {
+            soundGameObject = new GameObject("Sound");
+            soundAudioClipsQueue.Enqueue(soundGameObject);
+            audioSource = soundGameObject.AddComponent<AudioSource>();
+        }
+        else
+        {
+            soundGameObject = soundAudioClipsQueue.Dequeue();
+            soundAudioClipsQueue.Enqueue(soundGameObject);
+            audioSource = soundGameObject.GetComponent<AudioSource>();
+            audioSource.Stop();
+        }
+        audioSource.clip = GetAudioClip(_sound).audioClip;
+        audioSource.volume = GetAudioClip(_sound).volume;
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+
+    // Use this for sound effects where you want to hear the entire sound effect ex: Lightning
+    // WARNING: using this with sound effects that are repeated often may result in broken audio
+    public void PlayEntireSound(Sounds _sound)
     {
         //Create Audio Source Game Object
         GameObject soundGameObject = new GameObject("Sound");
         AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
-
-
         audioSource.clip = GetAudioClip(_sound).audioClip;
         audioSource.volume = GetAudioClip(_sound).volume;
         audioSource.PlayOneShot(audioSource.clip);
-
         Destroy(soundGameObject, audioSource.clip.length);
     }
 
